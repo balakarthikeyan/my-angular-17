4fcdf3830643b209f572087da493838465854a2b
Balakarthikeyan <balakarthikeyan07@gmail.com>
2024-08-18 13:10:28 +0530

    Initial Commit upgrading 16 to 17

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { CustomerCreateComponent } from './customer-create/customer-create.component';
+import { CustomerUpdateComponent } from './customer-update/customer-update.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'list'},
+  { path: 'list', component: CustomerListComponent},
+  { path: 'details/:id', component: CustomerDetailsComponent},
+  { path: 'create', component: CustomerCreateComponent},
+  { path: 'update', component: CustomerUpdateComponent},  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { CustomerCreateComponent } from './customer-create/customer-create.component';
+import { CustomerUpdateComponent } from './customer-update/customer-update.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerListComponent,
+    CustomerDetailsComponent,
+    CustomerCreateComponent,
+    CustomerUpdateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
